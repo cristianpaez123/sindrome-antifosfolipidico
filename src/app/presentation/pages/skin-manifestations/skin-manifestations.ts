90ae70e9458b3bc4179ba5eef7b3dc23efc67c5a
Cristian Paez <cristianpaezguerrero@gmail.com>
2026-02-06 00:35:46 -0500

    agrego modal a las imagenes manifestaciones cutaneas

--- src/app/presentation/pages/skin-manifestations/skin-manifestations.ts
+++ src/app/presentation/pages/skin-manifestations/skin-manifestations.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-skin-manifestations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skin-manifestations.html',
-  styleUrl: './skin-manifestations.css'
+  styleUrls: ['./skin-manifestations.css']
 })
 export class SkinManifestations {
+
+  isModalOpen = false;
+  activeImage: string | null = null;
+
+  openModal(imageSrc: string) {
+    console.log('CLICK OK', imageSrc);
+    this.activeImage = imageSrc;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.activeImage = null;
+  }
 
 }
